c3066eeda8590c6d624f584f4ac40956dcecab36
Ahmad El Hajj Chehade <ahmad.ehc@outlook.com>
2026-04-04 00:13:49 +0200

    Remove scroll indicator

--- components/hero.tsx
+++ components/hero.tsx
@@ -238,21 +238,6 @@ export function Hero() {
       >
         <AnimatedMountain />
       </motion.div>
-
-      {/* Scroll indicator */}
-      <motion.div
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
-        animate={{ y: [0, 10, 0] }}
-        transition={{ duration: 1.5, repeat: Infinity }}
-      >
-        <div className="w-6 h-10 rounded-full border-2 border-violet/50 flex justify-center pt-2">
-          <motion.div
-            className="w-1.5 h-1.5 rounded-full bg-violet"
-            animate={{ y: [0, 12, 0], opacity: [1, 0.5, 1] }}
-            transition={{ duration: 1.5, repeat: Infinity }}
-          />
-        </div>
-      </motion.div>
     </section>
   )
 }
