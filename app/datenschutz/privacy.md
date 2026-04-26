3bd0c49bb8db443e3ed8a94787ee6a0742edacfb
Andrin Schaller <65789180+codeofandrin@users.noreply.github.com>
2026-04-26 22:23:05 +0200

    Add markdown support for legal pages (#85)

---
+++ app/datenschutz/privacy.md
@@ -0,0 +1,42 @@
+# {{mainTitle}}
+
+{{introductionText}}
+
+<div className="spacer-lg"></div>
+
+## {{responsibilityTitle}}
+
+{{responsibilityText}}
+
+</br>
+
+{{responsibilityCompany}} \
+Suurstoffi 1 \
+6343 Rotkreuz \
+Schweiz
+
+<div className="spacer"></div>
+
+**{{responsibilityEmail}}:** [{{email-contact-hslu}}]({{email-url-contact-hslu}}) \
+**{{responsibilityPhone}}:** [{{phone-contact-hslu}}]({{phone-url-contact-hslu}}) \
+**{{responsibilityWebsite}}:** [{{website-contact-hslu}}]({{url-contact-hslu}})
+
+<div class="spacer-lg"></div>
+
+## {{dataCollectionTitle}}
+
+### {{logFilesTitle}}
+
+{{logFilesText1}}
+
+{{logFilesText2}}
+
+{{logFilesText3}}
+
+{{logFilesText4}}
+
+{{logFilesText5}}
+
+{{logFilesText6}}
+
+<div class="spacer"></div>
---
+++ components/ui/markdown-content.tsx
@@ -0,0 +1,45 @@
+import Markdown, { defaultUrlTransform } from "react-markdown"
+import rehypeRaw from "rehype-raw"
+import { Emails, PhoneNumbers, Urls } from "@/lib/constants"
+import { normalizeUrl } from "@/lib/helpers"
+
+interface MarkdownContentPropsType {
+  children: string
+  toReplace?: Record<string, string>
+}
+
+export default function MarkdownContent({ children, toReplace }: MarkdownContentPropsType) {
+  const replaceList: Record<string, string> = {
+    "email-contact-hslu": Emails.contactHSLU,
+    "email-url-contact-hslu": `mailto:${Emails.contactHSLU}`,
+    "phone-contact-hslu": PhoneNumbers.contactHSLU,
+    "phone-url-contact-hslu": `tel:${PhoneNumbers.contactHSLU.replaceAll(" ", "")}`,
+    "url-contact-hslu": Urls.contactHSLU,
+    "website-contact-hslu": normalizeUrl(Urls.contactHSLU),
+    ...toReplace
+  }
+
+  let content = children || ""
+  Object.entries(replaceList).forEach(([k, v]) => {
+    content = content.replaceAll("{{" + k + "}}", v)
+  })
+
+  return (
+    <div className="md-content">
+      <Markdown
+        rehypePlugins={[rehypeRaw]}
+        urlTransform={(url) => (url.startsWith("tel:") ? url : defaultUrlTransform(url))}
+        components={{
+          a(props: any) {
+            return (
+              <a href={props.href} target="_blank" className="text-primary hover:underline">
+                {props.children}
+              </a>
+            )
+          }
+        }}>
+        {content}
+      </Markdown>
+    </div>
+  )
+}
