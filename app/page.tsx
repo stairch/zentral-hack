4106f860776bf18c27f85fa83913eced6e9af5bf
Andrin Schaller <65789180+codeofandrin@users.noreply.github.com>
2026-08-19 18:00:07 +0200

    Change sponsors structure and move organizers up

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from "@/components/hero"
 import { About } from "@/components/about"
 import { Categories } from "@/components/categories"
 import { Schedule } from "@/components/schedule"
-import { Partners } from "@/components/partners"
+import { Partners, CoOrganisers } from "@/components/partners"
 import { FAQ } from "@/components/faq"
 import { CTA } from "@/components/cta"
 import { Footer } from "@/components/footer"
@@ -15,6 +15,7 @@ export default function Home() {
       <Navigation />
       <Hero />
       <About />
+      <CoOrganisers />
       <Suspense>
         <Categories />
       </Suspense>
